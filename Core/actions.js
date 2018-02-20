3978c71f1dfd715ae330e9106180a0fc81347feb
axeldenjean <34676803+axeldenjean@users.noreply.github.com>
2018-02-20 10:03:40 +0100

    debut actions (lanceur d'actions)

--- Core/actions.js
+++ Core/actions.js
@@ -1,6 +1,28 @@
-#!/usr/bin/env nod
+#!/usr/bin/env node
+
+
+var exec = require('child_process').exec, child;
+
+function child() {
+    exec('cat *.js bad_file | wc -l',
+        function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+}
+
 function ajouter() {
+    //ajout d'un fichier
     console.log("Ajout en cours")
+
+
+
+
+    child();
+
 }
 
 function supprimer() {
